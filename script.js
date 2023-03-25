49adad9a7ed91e16a2b17a6f899cda4f67687180
Hector Alejandro <hector.alejandro2221@gmail.com>
2023-03-24 23:50:11 -0600

    clase03 completada

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (e) => {
   e.preventDefault();
   const input = document.querySelector("[data-form-input]");
+  const value = input.value;
+  const list = document.querySelector("[data-list]");
+  const task = document.createElement("li");
+  task.classList.add("card");
+  input.value = "";
+
+  list.appendChild(task);
+  console.log(value);
 };
 
 btn.addEventListener("click", createTask);
