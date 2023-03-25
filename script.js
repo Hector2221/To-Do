2befb4908be4593dbc16d03a7696556fbd420c80
Hector Alejandro <hector.alejandro2221@gmail.com>
2023-03-25 00:33:08 -0600

    clase04 completada

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-const btn = document.querySelector("[data-form-btn]");
+//IIFE
+(() => {
+  const btn = document.querySelector("[data-form-btn]");
 
-const createTask = (e) => {
-  e.preventDefault();
-  const input = document.querySelector("[data-form-input]");
-  const value = input.value;
-  const list = document.querySelector("[data-list]");
-  const task = document.createElement("li");
-  task.classList.add("card");
-  input.value = "";
+  const createTask = (e) => {
+    e.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+    input.value = "";
 
-  list.appendChild(task);
-  console.log(value);
-};
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerHTML = value;
 
-btn.addEventListener("click", createTask);
+    taskContent.appendChild(titleTask);
+    task.appendChild(taskContent);
+    list.appendChild(task);
+  };
+
+  btn.addEventListener("click", createTask);
+
+  const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
+    return i;
+  };
+
+  const completeTask = (e) => {
+    const element = e.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+  };
+})();
