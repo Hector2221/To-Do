8de97d82b3a6096782fdb759fc980f7e5221166c
Hector Alejandro <hector.alejandro2221@gmail.com>
2023-03-25 00:52:16 -0600

    clase05 completada

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 //IIFE
 (() => {
   const btn = document.querySelector("[data-form-btn]");
@@ -19,22 +21,9 @@
 
     taskContent.appendChild(titleTask);
     task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
   };
 
   btn.addEventListener("click", createTask);
-
-  const checkComplete = () => {
-    const i = document.createElement("i");
-    i.classList.add("far", "fa-check-square", "icon");
-    i.addEventListener("click", completeTask);
-    return i;
-  };
-
-  const completeTask = (e) => {
-    const element = e.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-  };
 })();
